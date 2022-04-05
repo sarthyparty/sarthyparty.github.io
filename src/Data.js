29190e10b98fea6ac0f3ce7e6b709d6c1b2b8885
Sarthak Agrawal <agraw189@umn.edu>
2022-04-05 11:37:27 -0500

    added find my ep

--- src/Data.js
+++ src/Data.js
@@ -6,15 +6,15 @@ export const projects = [
     link: "https://github.com/sarthyparty/attendance-tracker"
   },
   {
-    name: "Personal Website",
-    description: "Coded my own website to showcase my projects. Turned out to be a lot harder than I thought.",
-    image: "website.png",
-    link: "https://github.com/sarthyparty/sarthyparty.github.io"
-  },
-  {
     name: "Soccer Match Predictions",
     description: "Used to TigerGraph to build a graph database trained with thousands of soccer matches. Then given a year, it is able to guess what the outcome of a match between two teams with a given year. This involved a lot of data processing in Jupyter Notebook using pandas as well.",
     image: "tg.png",
     link: "https://sarthavader.medium.com/predict-who-will-win-a-soccer-match-using-tigergraph-pandas-and-gsql-part-1-3-65c2fef665e"
+  },
+  {
+    name: "Find My EP",
+    description: "I formed and led a team to build a GPS app for my highschool. It is not yet available to download on the App Store. The path-finding algorithm is A star and I used SwiftUI and UIKit to create a visually-appealing UI.",
+    image: "gps.png",
+    link: "https://github.com/sarthyparty/Find-My-EP"
   }
 ]
